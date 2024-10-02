8ec64ddcd7768d961db51f52e2868ca3bb16e5e8
ZubairAhmed90 <106171902+ZubairAhmed90@users.noreply.github.com>
2024-10-02 12:23:23 +0500

    Update store.js

--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoslicer from "../Reducer/todoslicer";
+import todosReducer from "../Reducer/todoslicer";
 
 export const store =configureStore({
 
     reducer:{
-        todos :todoslicer,
+        todos :todosReducer,
     }
 })
